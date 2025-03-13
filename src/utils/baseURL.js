bd66f1a67a7404bf0fcc8752481b62e8f4bfaa7d
Alcides Polanco <alcidespg2002@gmail.com>
2025-03-13 07:57:29 -0400

    Adding back-end deploy domain

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-  return "http://localhost:3001";
+  // return "http://localhost:3001";
+  return "https://book-store-backend-beige.vercel.app";
 };
 
 export default getBaseUrl;
